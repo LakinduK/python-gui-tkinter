6553519f2f7b58ff4d0aefdd26b3374abb250374
LakinduK <lakindu1997@gmail.com>
2020-11-19 22:53:34 +0530

    add entry field

--- tkinter1.py
+++ tkinter1.py
@@ -2,13 +2,19 @@ import tkinter as tk
 window = tk.Tk()
 
 window.title("hello")
-window.geometry("400x600")
+window.geometry("400x400")
 
 # label
 title = tk.Label(text="hello world, welcome to tkinter")
-title.grid()
+title.grid(column=3, row=0)
 
-button = tk.Button(text="exit")
+# button
+button1 = tk.Button(text="exit")
+button1.grid(column=3, row=1)
+
+#entry field
+entry_field1 = tk.Entry()
+entry_field1.grid(column=0,row=1)
 
 
 # main window call
