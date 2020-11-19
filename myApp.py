6f90c0d044a4694a6d1c1847c3ab051a0ee20ffe
LakinduK <lakindu1997@gmail.com>
2020-11-20 00:01:12 +0530

    added readme

--- myApp.py
+++ myApp.py
@@ -12,8 +12,10 @@ def phrase_generator():
 
     phrases = ["hello ", "what's up ", "Aloha "]
 
+    # get user input fields to name var
     name = str(entry1.get())
     return phrases[random.randint(0, 2)] + name
+
 
 def phrase_display():
     greeting = phrase_generator()
@@ -24,6 +26,7 @@ def phrase_display():
 
     greeting_display.insert(tk.END, greeting)
 
+
 # label
 label1 = tk.Label(text="welcome to my app")
 label1.grid(column=0, row=0)
@@ -32,7 +35,7 @@ label2 = tk.Label(text="what is your name? ")
 label2.grid(column=0, row=1)
 
 
-# entries
+# entries/ user inputs
 entry1 = tk.Entry()
 entry1.grid(column=1, row=1)
 
---
+++ readme.md
@@ -0,0 +1,36 @@
+# Python GUI using tkinter
+
+this is a simple random pharase generator program using python tkinter to make GUIs in python
+Refer the tkinter documentation available at http://effbot.org/tkinterbook/
+
+### instructions
+
+install python
+https://python.org
+
+#### install tkinter
+
+- install tkinter(windows):
+
+```sh
+pip install tkinter
+```
+
+- install tkinter(linux):
+
+```sh
+sudo pip install tkinter
+```
+
+- run app
+  (open the folder in terminal and run app)
+
+```sh
+python tkinter1.py
+```
+
+OR
+
+```sh
+python myApp.py
+```
