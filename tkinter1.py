977da82c2c505198f1281881d8b6248a1e6654f1
LakinduK <lakindu1997@gmail.com>
2020-11-19 22:57:22 +0530

    added text field

--- tkinter1.py
+++ tkinter1.py
@@ -6,15 +6,19 @@ window.geometry("400x400")
 
 # label
 title = tk.Label(text="hello world, welcome to tkinter")
-title.grid(column=3, row=0)
+title.grid(column=0, row=0)
 
 # button
 button1 = tk.Button(text="exit")
-button1.grid(column=3, row=1)
+button1.grid(column=0, row=1)
 
-#entry field
+# entry field
 entry_field1 = tk.Entry()
-entry_field1.grid(column=0,row=1)
+entry_field1.grid(column=0, row=2)
+
+# text field
+text_field1 = tk.Text(master=window, height=10, width=40)
+text_field1.grid(column=0, row=3)
 
 
 # main window call
