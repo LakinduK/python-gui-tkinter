ffbc723b2f973b9e4024f0659d86a5ec93e3aed8
LakinduK <lakindu1997@gmail.com>
2020-11-19 23:31:16 +0530

    added base

--- myApp.py
+++ myApp.py
@@ -4,10 +4,26 @@ window = tk.Tk()
 window.title("My app")
 window.geometry("400x400")
 
+# Functions
 
 
 
 
 
+# label
+label1 = tk.Label(text="welcome to my app")
+label1.grid(column=0, row=0)
+
+label2 = tk.Label(text="what is your name? ")
+label2.grid(column=0, row=1)
+
+
+# entries
+entry1 = tk.Entry()
+entry1.grid(column=1, row=1)
+
+#button
+button1 = tk.Button()
+button1.grid(column=0,row=2)
 
 window.mainloop()
